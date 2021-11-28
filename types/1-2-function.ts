b198f0233cd566aaf6986c3cc687d35fcab81783
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-11-28 23:59:34 +0900

    함수에서 타입 이용하기

---
+++ types/1-2-function.ts
@@ -0,0 +1,29 @@
+{
+  // JavaScript 💩
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+  //TypeScript 👍
+  function tsAdd(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+  // JavaScript 💩
+  function jsFetchNum(id) {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  //TypeScript 👍
+  function tsFetchNum(id: string): Promise<number> {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+}
