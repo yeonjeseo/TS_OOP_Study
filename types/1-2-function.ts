f5bddb261f7c15fa595cee03efb5a99bb7536e29
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-11-29 00:15:56 +0900

    함수 타입 이용

--- types/1-2-function.ts
+++ types/1-2-function.ts
@@ -1,29 +1,51 @@
 {
-  // JavaScript 💩
-  function jsAdd(num1, num2) {
-    return num1 + num2;
-  }
-  //TypeScript 👍
-  function tsAdd(num1: number, num2: number): number {
-    return num1 + num2;
-  }
-  // JavaScript 💩
-  function jsFetchNum(id) {
-    // code ...
-    // code ...
-    // code ...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
+  // // JavaScript 💩
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+  // //TypeScript 👍
+  // function tsAdd(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+  // // JavaScript 💩
+  // function jsFetchNum(id) {
+  //   // code ...
+  //   // code ...
+  //   // code ...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
 
-  //TypeScript 👍
-  function tsFetchNum(id: string): Promise<number> {
-    // code ...
-    // code ...
-    // code ...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
+  // //TypeScript 👍
+  // function tsFetchNum(id: string): Promise<number> {
+  //   // code ...
+  //   // code ...
+  //   // code ...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  // JavaSript => TypeScript
+  // Optional Parameter
+  function printName(firstName: string, lastName?: string): void {
+    console.log(`Hello ${firstName} ${lastName}!`);
   }
+  printName("Yeonje", "Seo");
+  printName("Yeonje");
+  printName("Yeonje", undefined);
+
+  // Default Parameter
+  function printMessage(message: string = "Default message!"): void {
+    console.log(message);
+  }
+  printMessage();
+
+  // Rest Parameter
+  function printSum(...args: number[]): number {
+    console.log(args);
+    return args.reduce((prev, curr) => prev + curr, 0);
+  }
+  console.log(printSum(1, 2, 3, 4));
 }
