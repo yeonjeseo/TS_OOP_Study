5a36e91a827252698311dd4f87cb047e3c9c371e
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-12-16 12:12:15 +0900

    API 확인해보기

---
+++ 7.api/7-1-api.ts
@@ -0,0 +1,28 @@
+Array;
+
+type Student = {
+  passed: boolean;
+};
+
+const students: Student[] = [
+  { passed: true },
+  { passed: true },
+  { passed: true },
+];
+const result = students.every((students) => students.passed);
+
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+}
+class Dog extends Animal {
+  isDog: boolean = false;
+}
+
+const animals: Animal[] = [new Cat(), new Cat(), new Cat()];
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+
+console.log(animals.every<Cat>(isCat));
