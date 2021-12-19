97f341bb2f03bb35e6240dc7953d7e8f9aa1bc54
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-12-19 22:36:48 +0900

    union type을 이용항 error state 정의

--- 8.exception/8-2-handling.ts
+++ 8.exception/8-2-handling.ts
@@ -1,30 +1,32 @@
-class NetworkClient {
-  tryConnect(): void {
-    throw new Error("no network!");
-  }
-}
-
-class UserService {
-  constructor(private client: NetworkClient) {}
-  login() {
-    this.client.tryConnect();
-  }
-}
-
-class App {
-  constructor(private userService: UserService) {}
-  run() {
-    // 함수 첫 호출 부분에서 예외처리를 확실하게 하지 않을 거면 상위 클래스에서 예외처리를 하는 것이 좋음
-    // 위 주석과 같은 경우 상위 클래스에서 의미 있는 예외처리를 할 수 있음.
-    try {
-      this.userService.login();
-    } catch (error) {
-      // show dialog to user
-      console.log(error);
+{
+  class NetworkClient {
+    tryConnect(): void {
+      throw new Error("no network!");
     }
   }
+
+  class UserService {
+    constructor(private client: NetworkClient) {}
+    login() {
+      this.client.tryConnect();
+    }
+  }
+
+  class App {
+    constructor(private userService: UserService) {}
+    run() {
+      // 함수 첫 호출 부분에서 예외처리를 확실하게 하지 않을 거면 상위 클래스에서 예외처리를 하는 것이 좋음
+      // 위 주석과 같은 경우 상위 클래스에서 의미 있는 예외처리를 할 수 있음.
+      try {
+        this.userService.login();
+      } catch (error) {
+        // show dialog to user
+        console.log(error);
+      }
+    }
+  }
+  const client = new NetworkClient();
+  const service = new UserService(client);
+  const app = new App(service);
+  app.run();
 }
-const client = new NetworkClient();
-const service = new UserService(client);
-const app = new App(service);
-app.run();
