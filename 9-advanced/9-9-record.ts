79193e6c9e8a0f99bd71e396d75aaafa1e60b4f0
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-01-04 15:48:00 +0900

    etc

--- 9-advanced/9-9-record.ts
+++ 9-advanced/9-9-record.ts
@@ -5,9 +5,13 @@
   type Page = "home" | "about" | "contact";
 
   // Page와 PageInfo를 묶어주는 역할을 함
+  // map 자료 구조와 비슷
   const nav: Record<Page, PageInfo> = {
     home: { title: "Home" },
     about: { title: "About" },
     contact: { title: "Contact" },
   };
+
+  type Product = "cat" | "dog";
+  type NewProduct = Capitalize<Product>; //"Cat" | "Dog"
 }
